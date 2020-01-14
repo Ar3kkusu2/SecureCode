478898f4957909c9586de5888e151bc932faa15e
Alex <alex@example.com>
2020-01-14 12:43:35 +0100

    oauth

---
+++ BrokenAutenthication/Oauth/oauth.php
@@ -0,0 +1,23 @@
+<?php
+require '../composer/vendor/autoload.php';
+
+session_start();
+
+$client = new Google_Client();
+// segurtasun arazoa dago. Kredentzialak fitxategi batean gordeta dauzkagu eta
+// edozeinek irakurri ditzake 
+
+$client->setAuthConfig('client_secret.json');
+
+//$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
+// https://developers.google.com/identity/protocols/googlescopes?hl=en_US
+$client->addScope("https://www.googleapis.com/auth/userinfo.email");
+
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+  $client->setAccessToken($_SESSION['access_token']);
+  header('Location: ../php/login_ok.php');
+} else {
+  $folder = "/Workspace/SecureCode/BrokenAutenthication/Oauth/google-oauth2";
+  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . $folder . '/oauth_call.php';
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
