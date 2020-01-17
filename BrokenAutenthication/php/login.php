59789c59ed801d03d9beb4c18f7bcbd54f6fc245
Alex <alex@example.com>
2020-01-17 10:19:27 +0100

    joder siiiiiii

--- BrokenAutenthication/php/login.php
+++ BrokenAutenthication/php/login.php
@@ -2,26 +2,32 @@
 
 require 'db.php';
 
-$user =  strtolower($_POST['user']);
-$pass = $_POST['pass'];
+$user =  strtolower($_GET['user']);
+$pass = $_GET['pass'];
 
 //echo $user.'<br>'.$pass;
 //$hash = password_hash($pass, PASSWORD_DEFAULT);
 //echo $hash;
 
-$sql = "SELECT * FROM users WHERE user = '$user';";
+//$sql = "SELECT * FROM users WHERE user = '$user';";
 //echo $sql;
+$sql = "SELECT * FROM users WHERE user = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('s',$user);
+$stmt->execute();
 
-$result = $conn->query($sql);
+//$result = $conn->query($sql);
+//$row = $result->fetch_assoc();
+$result = $stmt->get_result();
 
-$row = $result->fetch_assoc();
-
-if (($row) && ($user == $row["user"]) && (password_verify($password, $row["pass"]))) {
+while ($row = $result->fetch_assoc()) {
+    if (($row) && ($user == $row["user"]) && (password_verify($pass, $row["pass"]))) {
     session_start();
     $_SESSION['user'] = $user;
     header('location: login_ok.php');
-} else{    
+    } else{    
     header('location: ../login.html');
+    }
 }
 
 $conn->close();
